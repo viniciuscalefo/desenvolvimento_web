4f1251727fae502d1b5aa772eb821fe8d6a5a7b3
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2023-07-10 16:33:53 -0300

    .
    .

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,57 +1,62 @@
+ var numberOfDrums=document.querySelectorAll(".drum").length;
 
-function clickAll(){
-    var numberOfDrums=document.querySelectorAll(".drum").length;
-    for(var n=0 ; n<numberOfDrums; n++){
+ for(var n=0 ; n<numberOfDrums; n++){
+
     document.querySelectorAll("button")[n].addEventListener("click",function(){ 
 
-        var buttonInnerHTML=this.innerHTML
-       switch(buttonInnerHTML){
-        case "w":
-            var tom1=new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-         
-        case "a":
-            var tom2=new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
+        var buttonInnerHTML=this.innerHTML;
 
-        case "s":
-            var tom3=new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
+        makeSound(buttonInnerHTML)
 
-        case "d":
-            var tom4=new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-
-        case "j":
-            var snare=new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-
-        case "k":
-            var crash=new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-
-        case "l":
-            var kick=new Audio("sounds/kick-bass.mp3");
-            kick.play();
-            break;
-
-            default:console.log()
-       }
-
-
-
-
-
-
-    })}
+    });
 }
-clickAll()
+//key down
+document.addEventListener("keydown",function(event){
+
+  makeSound(event.key)
+
+});
+
+function makeSound(key){
+    switch(key){
+            case "w":
+                var tom1=new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            
+            case "a":
+                var tom2=new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3=new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            case "j":
+                var snare=new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "k":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "l":
+                var kick=new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+                default:console.log()
+        }
+}
 
 /*ou
 usando direto o loop, sem usar uma function
