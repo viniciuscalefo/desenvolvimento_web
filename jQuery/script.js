d69b07b7fa78fa365fe73f4262dbec622a9f0c56
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2023-07-17 17:02:53 -0300

    starting with jQuery
    starting with jQuery

---
+++ jQuery/script.js
@@ -0,0 +1,3 @@
+$("h1").css("color","red");//equivalente a document.querySelector("h1").style.color="red";
+//em jQuey o $ serve tanto para substituir o document.queryselectAll quanto o queryselect
+$("button").text("right on")
