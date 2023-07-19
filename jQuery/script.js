ca91040dce35c3ddc90ac80f247051063b7796f8
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2023-07-19 07:35:55 -0300

    .
    .

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,3 +1,4 @@
 $("h1").css("color","red");//equivalente a document.querySelector("h1").style.color="red";
 //em jQuey o $ serve tanto para substituir o document.queryselectAll quanto o queryselect
 $("button").text("right on")
+$("img").attr("src")
